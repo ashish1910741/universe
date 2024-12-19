9995376f83005dae8859ea243ee5042e2369aef8
ashish1910741 <73703264+ashish1910741@users.noreply.github.com>
2024-12-20 02:37:03 +0530

    Add files via upload

---
+++ universe/src/test/java/com/world/universe/UniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.world.universe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
